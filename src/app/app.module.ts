a3158b2cde83bd5ffc7185dc5360e26fb47206aa
vrvivek443 <vivek@sixofour.in>
2026-02-02 09:54:01 +0530

    Message

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SideNavComponent } from './side-nav/side-nav.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { MemberlogoutComponent } from './memberlogout/memberlogout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SideNavComponent,
+    DashboardComponent,
+    MemberlogoutComponent
   ],
   imports: [
     BrowserModule,
